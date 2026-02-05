7ac939e495aad7f2d1001dd7db9bbf3ca65721fb
rishulafpl-svg <rishul.afpl@gmail.com>
2026-02-05 17:30:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from telegram.ext import Application, MessageHandler, filters, CommandHandler, C
 
 # ===== CONFIG =====
 BOT_TOKEN = '8463828441:AAExeLSEkpCQre2FaWmLfz1VnTOKV_RGcH8'
-APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwerJXVu3J5At_K_eqqb4bIQMtGQb0EohsyirXwTxVSLmLOMN6uF_ANNRcOa2OoZJJ2/exec'
+APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzW1Z_7oLt3a-aEUHHKxpm--yuFjBugWiRb4Kz5liDInPPU5lODRss3_D-amnP2YQfJ/exec'
 YOUR_USER_ID = 1345952228
 
 os.makedirs('downloads', exist_ok=True)
